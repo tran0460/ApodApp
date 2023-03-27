8ee8632a09795e57fc04329c2dce35202eb74d48
Son Tran <tran0460@algonquinlive.com>
2023-03-26 21:29:09 -0400

    add slide show delay values handling

--- ApodTestApp/SlideShowPage.xaml.cs
+++ ApodTestApp/SlideShowPage.xaml.cs
@@ -5,10 +5,17 @@ public partial class SlideShowPage : ContentPage
 	public SlideShowPage()
 	{
 		InitializeComponent();
+		SlideshowSlider.Value = Convert.ToDouble(App.SlideShowDelay);
 	}
 
     private void SlideshowSlider_ValueChanged(object sender, ValueChangedEventArgs e)
     {
+		App.SlideShowDelay = Convert.ToInt32(SlideshowSlider.Value);
 
+    }
+
+    private async void SlideshowBtn_Clicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//MainPage");
     }
 }
